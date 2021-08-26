5c991e982a2d76b8d660385425d0047f99b3aa10
TylerDurden75 <raytibo.j@gmail.com>
2021-08-26 18:48:09 +0200

    delete cart check

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addToCart } from "./../actions/cartActions";
+import { addToCart, removeFromCart } from "./../actions/cartActions";
 import MessageBox from "./../components/MessageBox";
 import { Link } from "react-router-dom";
 
@@ -23,6 +23,7 @@ export default function CartScreen(props) {
   };
   const removeFromCartHandler = (id) => {
     //delete action
+    dispatch(removeFromCart(id));
   };
 
   return (
