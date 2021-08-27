23525625523512f2f697521e2d33db253fc58689
TylerDurden75 <raytibo.j@gmail.com>
2021-08-27 11:09:55 +0200

    user connected to api

--- backend/routers/UserRouter.js
+++ backend/routers/UserRouter.js
@@ -2,6 +2,8 @@ import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import data from "../data.js";
 import User from "./../models/userModel.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils.js";
 
 const UserRouter = express.Router();
 
@@ -12,6 +14,26 @@ UserRouter.get(
     //await User.remove({})
     const createdUsers = await User.insertMany(data.users);
     res.send({ createdUsers });
+  })
+);
+
+UserRouter.post(
+  "/signin",
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: "Invalid email or password" });
   })
 );
 
